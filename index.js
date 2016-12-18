a1455cb591d681565a13f80eff07eb45bd38ea01
Jorge Bucaran <mail@jorgebucaran.com>
2016-12-19 02:50:28 +0900

    Drop virtual dom in favor of snabbdom.
    * Elm arch implementation is more pure this time, but
      more code was needed to transform hyperx attrs into
      a data object that snabbdom/h can use.
    
    * The reason to switch to Snabbdom is due to the project
      significantly better results in some known vdom benchmarks,
      smaller size and no deps (as opposed to virtual-dom that
      had 8 deps, at the time of writing this commit).
    
    * See also http://vdom-benchmark.github.io/vdom-benchmark

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-const { h, create, diff, patch } = require("virtual-dom")
-const html = require("hyperx")(h)
+const h = require("snabbdom/h")
+const html = require("hyperx")((tagName, attrs, children) => {
+    const keys = Object.keys(attrs), data = {
+        class: {},
+        props: {},
+        on: {},
+        style: {}
+    }
 
-const app = (model, view, reducers, container) => {
+    let i = keys.length
+
+    while (~--i) {
+        const key = keys[i], value = attrs[key]
+
+        switch (key) {
+            case undefined:
+                break
+
+            case "className":
+                value.split(" ").forEach(cls => {
+                    data.class[cls] = true
+                })
+                break
+
+            case "style":
+                Object.assign(data.style, value)
+                break
+
+            default:
+                if (0 === key.indexOf("on")) {
+                    data.on[key.substr(2)] = value
+                } else {
+                    data.props[key] = value
+                }
+        }
+    }
+
+    return h(tagName, data, children)
+})
+
+const snabbdom = require("snabbdom")
+const patch = snabbdom.init([
+    require("snabbdom/modules/class"),
+    require("snabbdom/modules/props"),
+    require("snabbdom/modules/style"),
+    require("snabbdom/modules/eventlisteners")
+])
+
+const main = (model, view, reducers, container) => {
     const update = (model, action) => {
         const type = action.type === undefined
             ? action
@@ -12,39 +57,15 @@ const app = (model, view, reducers, container) => {
                 ? model
                 : reducers[type](model, action)
     }
-    const dispatch = action => render(update(state.model, action))
-    const tree = view(model, dispatch)
-    const state = {
-        model,
-        tree,
-        element: create(tree),
-        lockRedraw: false,
-        lockRender: false,
-    }
-    const render = model => {
-        if (state.lockRender) {
-            return
-        }
-        if (!state.lockRedraw) {
-            state.lockRedraw = true
-            requestAnimationFrame(_ => {
-                state.lockRender = true
-                state.lockRedraw = false
 
-                const tree = view(state.model, dispatch)
-                state.element = patch(state.element, diff(state.tree, tree))
-                state.tree = tree
-
-                state.lockRender = false
-            })
-        }
-        state.model = model
+    const render = (model, view, update, lastNode) => {
+        const nextNode = view(model, action =>
+            render(update(model, action), view, update, nextNode))
+        patch(lastNode, nextNode)
     }
-    (container ? container : document.body).appendChild(state.element)
+
+    return render(model, view, update, container
+        ? container
+        : document.body.appendChild(document.createElement("div"))
+    )
 }
-
-module.exports = { html, app }
-
-
-
-

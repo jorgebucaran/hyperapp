88aff5c6c5ba3d5bac5bc8f4ccc379a85420c2d5
Jorge Bucaran <jbucaran@gmail.com>
2016-12-19 23:19:36 +0900

    Simplify example.

--- examples/hello/index.js
+++ examples/hello/index.js
@@ -1,6 +1,3 @@
 const { app, html } = require("flea")
 
-const model = "hello, world"
-const view = model => html`<h1>${model}</h1>`
-
-app(model, view)
+app("Hello!", model => html`<h1>${model}</h1>`)

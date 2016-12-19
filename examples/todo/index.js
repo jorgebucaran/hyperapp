92cb8a65d523c6a8cb6b8c48516a4922dd401810
Jorge Bucaran <jbucaran@gmail.com>
2016-12-19 22:30:45 +0900

    Rewrite todo example.

--- examples/todo/index.js
+++ examples/todo/index.js
@@ -1,109 +1,88 @@
 const { html, app } = require("flea")
 
-const FILTER = { All: 0, Active: 1, Completed: 2 }
+const FilterInfo = { All: 0, Todo: 1, Done: 2 }
 
 const model = {
     todos: [],
-    text: "",
-    filter: {
-        values: ["All", "Active", "Completed"],
-        selected: 0
-    }
-}
-
-const update = ({ todos, text, filter }, msg) => {
-    switch (msg.type) {
-        case "ADD_TODO":
-            return {
-                todos: todos.concat({
-                    id: todos.length + 1,
-                    value: text,
-                    completed: false
-                }),
-                text: "",
-                filter
-            }
-
-        case "TOGGLE_TODO":
-            const newTodos = todos.map(t => t.id === msg.id
-                ? Object.assign({}, t, { completed: !t.completed })
-                : t)
-
-            return {
-                todos: newTodos,
-                text,
-                filter
-            }
-
-        case "UPDATE_TEXT":
-            return {
-                todos,
-                text: msg.value,
-                filter
-            }
-
-        case "UPDATE_FILTER":
-            return {
-                todos,
-                text,
-                filter: Object.assign({}, filter, { selected: msg.value })
-            }
-
-        default:
-            return model
-    }
+    filter: FilterInfo.All,
+    input: "",
+    placeholder: "Add new todo"
 }
 
 const view = (model, dispatch) => {
-    const addHandler = e =>
-        dispatch({ type: "ADD_TODO" }, model)
-
-    const toggleHandler = id =>
-        dispatch({ type: "TOGGLE_TODO", id }, model)
-
-    const editHandler = e =>
-        dispatch({ type: "UPDATE_TEXT", value: e.target.value }, model)
-
-    const filterHandler = i =>
-        dispatch({ type: "UPDATE_FILTER", value: i }, model)
-
-    const listItem = t => {
-        const value = (model.filter.selected === FILTER.Completed && t.completed)
-            ? t.value
-            : (model.filter.selected === FILTER.Active && !t.completed)
-                ? t.value
-                : (model.filter.selected === FILTER.All)
-                    ? t.value
-                    : ""
-
-        if (value === "") return
-
-        return html`
-            <li
-                style=${{ textDecoration: t.completed ? "line-through" : "none" }}
-                onclick=${_ => toggleHandler(t.id)}>${value}</li>
-        `
-    }
-
-    const filterLink = _ => model.filter.values.map((v, i) =>
-        html`
-            <span>
-                ${(i !== model.filter.selected
-                ? html`<a href="#" onclick=${e => filterHandler(i, e.preventDefault())}>${v}</a>`
-                : v)}
-            </span>
-        `)
-
     return html`
         <div>
-            <input onchange=${editHandler} value=${model.text}/>
-            <button onclick=${addHandler}>Add</button>
-            <ul>
-                ${model.todos.map(listItem)}
-            </ul>
-            ${filterLink()}
-        </div>
-    `
+            <h1>Todo</h1>
+            <p>
+                Show: ${
+                Object.keys(FilterInfo)
+                    .filter(key => FilterInfo[key] !== model.filter)
+                    .map(key => html`
+                        <span><a href="#" onclick=${_ => dispatch({
+                            type: "FILTER",
+                            value: FilterInfo[key]
+                        })}>${key}</a> </span>
+                    `)}
+            </p>
+
+            <p><ul>
+                ${model.todos
+                    .filter(t =>
+                        model.filter === FilterInfo.Done
+                            ? t.done :
+                        model.filter === FilterInfo.Todo
+                            ? !t.done :
+                        model.filter === FilterInfo.All)
+                    .map(t => html`
+                        <li style=${{
+                                textDecoration: t.done
+                                    ? "line-through"
+                                    : "none"
+                            }}
+                            onclick=${e => dispatch({
+                                type: "TOGGLE",
+                                value: t.done,
+                                id: t.id
+                            })}>${t.value}
+                        </li>`)}
+            </ul></p>
+
+            <p>
+                <input
+                    onkeyup=${ e => e.keyCode === 13
+                        ? dispatch({ type: "ADD" })
+                        : ""
+                    }
+                    oninput=${e => dispatch({
+                        type: "INPUT",
+                        value: e.target.value
+                    })}
+                    type="text"
+                    value=${model.input}
+                    placeholder=${model.placeholder}
+                />
+                <button onclick=${_ => dispatch({ type: "ADD" })}>add</button>
+            </p>
+        </div>`
+}
+
+const update = {
+    ADD: model => Object.assign({}, model, {
+        input: "", // reset input
+        todos: model.todos.concat({
+            done: false,
+            value: model.input,
+            id: model.todos.length + 1
+        })
+    }),
+    TOGGLE: (model, { id, value }) => Object.assign({}, model, {
+        todos: model.todos.map(t =>
+            id === t.id
+                ? Object.assign({}, t, { done: !value })
+                : t)
+    }),
+    INPUT: (model, { value }) => Object.assign({}, model, { input: value }),
+    FILTER: (model, { value }) => Object.assign({}, model, { filter: value })
 }
 
 app(model, view, update)

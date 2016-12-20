97e8862c0f4680ee2eb0fc0a45a41dbedae5e5bb
Jorge Bucaran <mail@jorgebucaran.com>
2016-12-21 01:40:38 +0900

    Add subscriptions / event dispatchers. Invoke on DOM ready.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const h = require("snabbdom/h")
 const html = require("hyperx")((tagName, attrs, children) => {
     const keys = Object.keys(attrs), data = {
-        class: {},
-        props: {},
-        style: {},
-        on: {}
+        class: {}, props: {}, style: {}, attrs: {}, on: {}
     }
 
     let i = keys.length
@@ -12,58 +9,61 @@ const html = require("hyperx")((tagName, attrs, children) => {
     while (~--i) {
         const key = keys[i], value = attrs[key]
 
-        if (key === undefined /* || value === undefined */) {
-            console.log(`Undefined key (${key}) or value (${value})`, tagName, attrs)
+        if (key === undefined) {
             continue
         }
 
-        switch (key) {
-            case "className":
-                value.split(" ").forEach(cls => {
-                    data.class[cls] = true
-                })
-                break
+        if (key === "className") {
+            value.split(" ").forEach(cls => data.class[cls] = true)
 
-            case "style":
-                Object.assign(data.style, value)
-                break
+        } else if (key === "style") {
+            data.style = value
 
-            default:
-                if (0 === key.indexOf("on")) {
-                    data.on[key.substr(2)] = value
-                } else {
-                    data.props[key] = value === "true"
-                        ? true : value === "false" ? false : value
-                }
+        } else if (0 === key.indexOf("on")) {
+            data.on[key.substr(2)] = value
+
+        } else {
+            data.props[key] = data.attrs[key] = value === "true"
+                ? true : value === "false" ? false : value
         }
     }
 
     return h(tagName, data, children ? [].concat(...children) : children)
 })
-
 const snabbdom = require("snabbdom")
 const patch = snabbdom.init([
     require("snabbdom/modules/class"),
     require("snabbdom/modules/props"),
     require("snabbdom/modules/style"),
+    require("snabbdom/modules/attributes"),
     require("snabbdom/modules/eventlisteners")
 ])
 
-const app = (model, view, reducers, container) => {
-    const update = (model, action) => {
-        const type = action.type === undefined
-            ? action
-            : action.type
-        return typeof reducers === "function"
-            ? reducers(model, action)
-            : reducers[type] === undefined
-                ? model
-                : reducers[type](model, action)
+const app = (model, view, reducers, subscriptions, container) => {
+    if (view === undefined) {
+        view = model.view
+        reducers = model.reducers || model.update
+        subscriptions = model.subscriptions
+        container = model.container
+        model = model.model
     }
 
+    const update = typeof reducers === "function"
+        ? reducers
+        : (model, msg) => {
+            const type = msg.type === undefined ? msg : msg.type
+            return reducers[type] === undefined
+                ? model
+                : reducers[type](model, msg)
+        }
+
     const render = (model, view, update, lastNode) => {
-        const nextNode = view(model, action =>
-            render(update(model, action), view, update, nextNode))
+        const send = msg => render(update(model, msg), view, update, nextNode)
+        const nextNode = view(model, send)
+
+        if (subscriptions)
+            document.addEventListener("DOMContentLoaded", _ =>
+                Object.keys(subscriptions).forEach(key => subscriptions[key](send)))
 
         patch(lastNode, nextNode)
     }

03b28db81686b8a8528eb949d990a6c40f9cde79
Jorge Bucaran <mail@jorgebucaran.com>
2016-12-21 01:42:23 +0900

    Increment by one. Show how to use disabled attribute with boolean var.

--- examples/dir/counter/index.js
+++ examples/dir/counter/index.js
@@ -4,13 +4,13 @@ const model = 0
 
 const view = (model, dispatch) => html`
     <div>
-        <h1>${model}</h1>
         <button onclick=${_ => dispatch("INCREMENT")}>+</button>
-        <button onclick=${_ => dispatch("DECREMENT")}>-</button>
+        <p>${model}</p>
+        <button onclick=${_ => dispatch("DECREMENT")} disabled=${model <= 0}>-</button>
     </div>`
 
 const update = {
-    INCREMENT: model => model + 2,
+    INCREMENT: model => model + 1,
     DECREMENT: model => model - 1
 }
 

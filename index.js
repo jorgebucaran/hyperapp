6f8de223fd9af1f1066905e95c29fd91b4a04cd4
Jorge Bucaran <jbucaran@gmail.com>
2016-12-22 00:13:18 +0900

    Rewrite in ES5 syntax for better browser support out of the box.

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
-const h = require("snabbdom/h")
-const html = require("hyperx")((tagName, attrs, children) => {
-    const keys = Object.keys(attrs), data = {
-        class: {}, props: {}, style: {}, attrs: {}, on: {}
-    }
+var snabbdom = require("snabbdom")
+var h = require("snabbdom/h")
 
-    let i = keys.length
-
-    while (~--i) {
-        const key = keys[i], value = attrs[key]
-
-        if (key === undefined) {
-            continue
-        }
-
-        if (key === "className") {
-            value.split(" ").forEach(cls => data.class[cls] = true)
-
-        } else if (key === "style") {
-            data.style = value
-
-        } else if (0 === key.indexOf("on")) {
-            data.on[key.substr(2)] = value
-
-        } else {
-            data.props[key] = data.attrs[key] = value === "true"
-                ? true : value === "false" ? false : value
-        }
-    }
-
-    return h(tagName, data, children ? [].concat(...children) : children)
-})
-const snabbdom = require("snabbdom")
-const patch = snabbdom.init([
+var patch = snabbdom.init([
     require("snabbdom/modules/class"),
     require("snabbdom/modules/props"),
     require("snabbdom/modules/style"),
@@ -39,7 +9,34 @@ const patch = snabbdom.init([
     require("snabbdom/modules/eventlisteners")
 ])
 
-const app = (model, view, reducers, subs, container) => {
+var html = require("hyperx")(function (tagName, attrs, children) {
+    var keys = Object.keys(attrs), i = keys.length
+    var data = {
+        class: {}, props: {}, style: {}, attrs: {}, on: {}
+    }
+
+    while (~--i) {
+        var key = keys[i], value = attrs[key]
+        if (key === undefined) continue
+
+        if (key === "className") {
+            value.split(" ").forEach(function(cls) {
+                data.class[cls] = true
+            })
+        } else if (key === "style") {
+            data.style = value
+        } else if (0 === key.indexOf("on")) {
+            data.on[key.substr(2)] = value
+        } else {
+            data.props[key] = data.attrs[key] = value === "true"
+                ? true : value === "false" ? false : value
+        }
+    }
+
+    return h(tagName, data, children ? [].concat.apply([], children) : children)
+})
+
+function app(model, view, reducers, subs, container) {
     if (view === undefined) {
         view = model.view
         reducers = model.reducers || model.update
@@ -48,24 +45,32 @@ const app = (model, view, reducers, subs, container) => {
         model = model.model
     }
 
-    const update = typeof reducers === "function"
-        ? reducers : (model, msg, data) => reducers[msg](model, data)
-
-    const render = (model, view, update, lastNode) => {
-        patch(lastNode, container = view(model, dispatch))
-        if (subs) {
-            document.addEventListener("DOMContentLoaded", _ =>
-                Object.keys(subs).forEach(key => subs[key](dispatch, model)))
+    var update = typeof reducers === "function"
+        ? reducers
+        : function(model, msg, data) {
+            return reducers[msg](model, data)
         }
+
+    function render(model, view, update, lastNode) {
+        patch(lastNode, container = view(model, dispatch))
+
+        subs && document.addEventListener("DOMContentLoaded", function () {
+            Object.keys(subs).forEach(function (key) {
+                subs[key](dispatch, model)
+            })
+        })
     }
 
-    const dispatch = (msg, data) =>
+    function dispatch(msg, data) {
         render(model = update(model, msg, data), view, update, container)
+        return model
+    }
 
     return render(model, view, update, container
-        ? container : document.body.appendChild(document.createElement("div"))
+        ? container
+        : document.body.appendChild(document.createElement("div"))
     )
 }
 
-module.exports = { html, app }
+module.exports = { html: html, app: app }
 

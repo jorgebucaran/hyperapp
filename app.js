cc86d20cbad18dac387d77bb5c9fd011b03720f7
Jorge Bucaran <mail@jorgebucaran.com>
2016-12-30 03:44:40 +0900

    Allow reducers to return part of a model.
    In this case the returned object will be merged into
    the current model.

--- app.js
+++ app.js
@@ -6,70 +6,87 @@ var patch = require("snabbdom").init([
     require("snabbdom/modules/eventlisteners")
 ])
 
-module.exports = function app(options) {
+module.exports = function app (options) {
     var model = options.model,
         view = options.view,
-        reducers = options.reducers || options.update || {},
-        subs = options.subscriptions || {},
+        reducers = options.update || {},
         effects = options.effects || {},
+        subs = options.subs || {},
         hooks = options.hooks || {},
         root = options.root || document.body.appendChild(document.createElement("div"))
 
     var update = function (model, name, data) {
         var reduce = reducers[name]
-        if (typeof reduce !== "function") {
-            throw TypeError(name + " is not a reducer or effect")
-        }
         return reduce(model, data)
     }
 
-    Object
-        .keys(reducers)
-        .concat(Object.keys(effects))
-        .forEach(function (name) {
-            if (reducers[name] !== undefined && effects[name] !== undefined) {
-                throw TypeError(name + " is already defined as a reducer or effect")
-            }
-            dispatch[name] = function (data) {
-                dispatch(name, data)
-            }
-        })
+    for (var name in reducers) {
+        wrap(dispatch, name)
+    }
+
+    for (var name in effects) {
+        if (dispatch[name] !== undefined) {
+            throw TypeError(name + " is already defined as a reducer or effect")
+        }
+        wrap(dispatch, name)
+    }
 
     document.addEventListener("DOMContentLoaded", function () {
-        Object.keys(subs).forEach(function (key) {
+        for (var key in subs) {
             subs[key](model, dispatch, error)
-        })
+        }
     })
 
     render(model, view, update, root)
 
-    function render(model, view, update, lastNode) {
-        if (view === undefined) {
-            return
+    function render (model, view, update, lastNode) {
+        if (view !== undefined) {
+            patch(lastNode, root = view(model, dispatch))
         }
-        patch(lastNode, root = view(model, dispatch))
     }
 
-    function error(err) {
+    function error (err) {
         if (hooks.onError === undefined) {
             throw err
         }
         hooks.onError(err)
     }
 
-    function dispatch(name, data) {
+    function dispatch (name, data) {
         if (hooks.onAction !== undefined) {
             hooks.onAction(name, data)
         }
+
         var effect = effects[name]
+
         if (typeof effect === "function") {
             effect(model, dispatch, error)
         } else {
             var lastModel = model
-            render(model = update(model, name, data), view, update, root)
+
+            render(model = merge(model, update(model, name, data)), view, update, root)
+
             if (hooks.onUpdate !== undefined) {
                 hooks.onUpdate(lastModel, model, data)
             }
+        }
+    }
+
+    function merge (target, source) {
+        if (typeof source === "string") {
+            return source
+        }
+
+        for (var key in source) {
+            target[key] = source[key]
+        }
+
+        return key === undefined ? source : target
+    }
+
+    function wrap (func, key) {
+        func[key] = function (data) {
+            func(key, data)
         }
     }
 }

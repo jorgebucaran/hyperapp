c827e2adffc10c35bb2ca18eae47523433db5f23
Jorge Bucaran <jbucaran@gmail.com>
2017-02-08 01:09:58 +0900

    Remove buble.

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,7 @@
-import buble from "rollup-plugin-buble"
 import uglify from "rollup-plugin-uglify"
 import cjs from "rollup-plugin-commonjs"
 import resolve from "rollup-plugin-node-resolve"
 
 export default {
-    plugins: [
-        buble(),
-        cjs(),
-        resolve(),
-        uglify()
-    ]
+    plugins: [ cjs(), resolve(), uglify() ]
 }

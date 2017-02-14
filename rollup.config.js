8d6126a7b9dee6824e7e45765f669cad45cb3a4a
Jorge Bucaran <jbucaran@gmail.com>
2017-02-15 00:50:21 +0900

    We only need to uglify at the moment since we're using ES modules now.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import uglify from "rollup-plugin-uglify"
-import cjs from "rollup-plugin-commonjs"
-import resolve from "rollup-plugin-node-resolve"
 
 export default {
-    plugins: [ cjs(), resolve(), uglify() ]
+    plugins: [
+        uglify()
+    ]
 }
+

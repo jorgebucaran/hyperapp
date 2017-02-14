30018cff5b64782ee83d40b62f15ae26b7f4b240
Jorge Bucaran <jbucaran@gmail.com>
2017-02-15 05:53:07 +0900

    Optimize uglifyjs's output via options.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,11 @@ import uglify from "rollup-plugin-uglify"
 
 export default {
     plugins: [
-        uglify()
+        uglify({
+            compress: {
+                collapse_vars: true,
+                pure_funcs: ["Object.defineProperty"]
+            }
+        })
     ]
 }
-

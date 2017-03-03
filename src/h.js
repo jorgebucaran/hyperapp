f79a4b47336e472ff3223445c1a9ff95b72cefc7
Jorge Bucaran <mail@jorgebucaran.com>
2017-03-04 02:36:48 +0900

    Prefer for loop over for..in in some places.

--- src/h.js
+++ src/h.js
@@ -1,60 +1,65 @@
 var i, node, children, stack = []
 
 export default function (tag, data) {
-	var canConcat, oldCanConcat
+  var canConcat, oldCanConcat
 
-	children = []
-	i = arguments.length
+  children = []
+  i = arguments.length
 
-	while (i-- > 2) {
-		stack.push(arguments[i])
-	}
+  while (i-- > 2) {
+    stack.push(arguments[i])
+  }
 
-	while (stack.length) {
-		if (Array.isArray(node = stack.pop())) {
-			i = node.length
+  while (stack.length) {
+    if (Array.isArray(node = stack.pop())) {
+      i = node.length
 
-			while (i--) {
-				stack.push(node[i])
-			}
-		} else if (node != null && node !== true && node !== false) {
-			if (typeof node === "number") {
-				node = node + ""
-			}
+      while (i--) {
+        stack.push(node[i])
+      }
+    } else if (node != null && node !== true && node !== false) {
+      // Ignore nulls and booleans; this is conditional rendering.
 
-			canConcat = typeof node === "string"
+      if (typeof node === "number") {
+        node = node + ""
+      }
 
-			if (canConcat && oldCanConcat) {
-				children[children.length - 1] += node
-			} else {
-				children.push(node)
-				oldCanConcat = canConcat
-			}
-		}
-	}
+      // Concatenate contiguous number/string nodes into one string.
+      // The idea is to avoid creating unnecessary text nodes.
 
-	if (typeof tag === "function") {
-		return tag(data, children)
-	}
+      canConcat = typeof node === "string"
 
-	if (tag === "svg") {
-		svg(tag, data, children)
-	}
+      if (canConcat && oldCanConcat) {
+        children[children.length - 1] += node
+      } else {
+        children.push(node)
+        oldCanConcat = canConcat
+      }
+    }
+  }
 
-	return {
-		tag: tag,
-		data: data || {},
-		children: children
-	}
+  if (typeof tag === "function") {
+    return tag(data, children)
+  }
+
+  if (tag === "svg") {
+    svg(tag, data, children)
+  }
+
+  return {
+    tag: tag,
+    data: data || {},
+    children: children
+  }
 }
 
 function svg(tag, data, children) {
-	data.ns = "http://www.w3.org/2000/svg"
+  data.ns = "http://www.w3.org/2000/svg"
 
-	for (var i = 0; i < children.length; i++) {
-		var node = children[i]
-		if (node.data) {
-			svg(node.tag, node.data, node.children)
-		}
-	}
+  for (var i = 0; i < children.length; i++) {
+    var node = children[i]
+    if (node.data) {
+      svg(node.tag, node.data, node.children)
+    }
+  }
 }

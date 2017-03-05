2b0ad8502b97e75f3e565b9b617a0502d77b7d6d
Luke Jackson <lukejacksonn@gmail.com>
2017-03-05 02:47:16 -0500

    Internal routing hook for href=/*

--- src/router.js
+++ src/router.js
@@ -26,7 +26,25 @@ export default function (options) {
         addEventListener("popstate", function () {
           actions.router.match(location.pathname)
         })
-      }
+      },
+      function (_, actions) {
+        addEventListener("click", function (e) {
+          if (e.metaKey || e.shiftKey || e.ctrlKey || e.altKey) return
+          var target = e.target
+          while (target && target.localName !== "a") {
+            target = target.parentNode
+          }
+          if (target && target.host === location.host && !target.hasAttribute("data-no-routing")) {
+            var element = document.querySelector(target.hash === "" ? element : target.hash)
+            if (element) {
+              element.scrollIntoView(true)
+            } else {
+              e.preventDefault()
+              actions.router.go(target.pathname)
+            }
+          }
+        })
+      },
     ]
   }
 }

f28193e0d949332c13168abeaa47b27dca89183d
ngryman <ngryman@gmail.com>
2017-04-15 20:20:35 +0200

    test with an array child

--- perf/h.js
+++ perf/h.js
@@ -16,3 +16,11 @@ bench("vnode with a single child 1M times", b => {
   }
   b.end()
 })
+
+bench("vnode with a single array child 1M times", b => {
+  b.start()
+  for (let i = 0; i < 10000000; i++) {
+    h("div", {}, [["foo"]])
+  }
+  b.end()
+})

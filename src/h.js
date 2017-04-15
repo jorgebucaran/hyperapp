d586d6fbfc148f023dcb484cb188bccb822964e9
ngryman <ngryman@gmail.com>
2017-04-15 22:05:49 +0200

    Improve perfs

--- src/h.js
+++ src/h.js
@@ -1,28 +1,28 @@
+function isValid(val) {
+  return (val != null && val !== true && val !== false)
+}
+
+function sanitize(val) {
+  return typeof val === 'number' ? val + '' : val
+}
+
 function addChild(children, val) {
   if (Array.isArray(val)) {
     for (var j = 0; j < val.length; j++) {
       children.push(val[j])
     }
   }
-  else if (val != null && val !== true && val !== false) {
-    if (typeof val === 'number') {
-      val = val + ''
-    }
-    children.push(val)
+  else if (isValid(val)) {
+    children.push(sanitize(val))
   }
 }
 
 export default function(tag, data, values) {
-  var children = []
-
-  if (Array.isArray(values)) {
-    for (var i = 0; i < values.length; i++) {
-      addChild(children, values[i])
-    }
-  }
-  else {
-    addChild(children, values)
-  }
+  var children = Array.isArray(values)
+    ? values
+    : isValid(values)
+      ? [sanitize(values)]
+      : []
 
   for (var i = 3; i < arguments.length; i++) {
     addChild(children, arguments[i])

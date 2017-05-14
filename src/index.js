a6cf50301f194536d8141aa653ad478d9bf6f65a
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-05-14 15:44:48 -0700

    Render virtual dom object to html string

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import h from "./h"
 import app from "./app"
 import Router from "./router"
+import renderToString from "./render-to-string"
 
-export { h, app, Router }
+export { h, app, renderToString, Router }
---
+++ test/render-to-string.test.js
@@ -0,0 +1,71 @@
+import { renderToString } from "../src"
+
+const RenderTest = trees =>
+  trees.forEach(tree => expect(renderToString(tree.vnode)).toBe(tree.html))
+
+const createVNode = vnode => Object.assign({ tag: "div", data: {}, children: [] }, vnode)
+
+test("empty element", () => {
+   RenderTest([
+     {
+       vnode: createVNode(),
+       html: `<div></div>`
+     },
+     {
+       vnode: createVNode({ tag: "h1" }),
+       html: `<h1></h1>`
+     }
+   ])
+})
+
+test("attributes", () => {
+  RenderTest([
+    {
+      vnode: createVNode({ data: { id: "a" } }),
+      html: `<div id="a"></div>`
+    },
+    {
+      vnode: createVNode({ data: { class: "a" } }),
+      html: `<div class="a"></div>`
+    },
+    {
+      vnode: createVNode({ data: { class: "a", id: "a" }}),
+      html: `<div class="a" id="a"></div>`
+    }
+  ])
+})
+
+test("style", () => {
+  RenderTest([
+    {
+      vnode: createVNode({ data: { style: { margin: 0 } } }),
+      html: `<div style="margin:0;"></div>`
+    },
+    {
+      vnode: createVNode({ data: { style: { backgroundColor: "papayawhip" } } }),
+      html: `<div style="background-color:papayawhip;"></div>`
+    }
+  ])
+})
+
+test("primitive children", () => {
+  RenderTest([
+    {
+      vnode: createVNode({ children: ["foo", "bar"] }),
+      html: `<div>foobar</div>`
+    }
+  ])
+})
+
+test("vnode children", () => {
+  RenderTest([
+    {
+      vnode: createVNode({
+        children: [
+          createVNode({ tag: "h1" })
+        ]
+      }),
+      html: `<div><h1></h1></div>`
+    }
+  ])
+})

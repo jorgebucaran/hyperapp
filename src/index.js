f70b9b5caadc463e5f615f8af6b8f29e5de32089
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-05-16 10:50:38 -0700

    Remove renderToString from index.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import h from "./h"
 import app from "./app"
 import Router from "./router"
-import renderToString from "./render-to-string"
 
-export { h, app, renderToString, Router }
+export { h, app, Router }

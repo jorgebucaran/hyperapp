ca4c1df8fe60acd9c6bb00837aca297b607d2e29
Quentin Gerodel <gerodel.quentin@gmail.com>
2017-07-04 15:00:08 +0200

    [typing] Format declaration file

--- hyperapp.d.ts
+++ hyperapp.d.ts
@@ -1,62 +1,149 @@
 declare module "hyperapp" {
-
   export interface VNodeData {
-    [key: string]: any;
+    [key: string]: any
   }
 
   export interface VNode {
-    tag: string;
-    data: VNodeData;
-    children: string | VNode | Array<VNode>;
+    tag: string
+    data: VNodeData
+    children: string | VNode | Array<VNode>
   }
 
-  export type viewFunction<State, Actions> = (state: State, actions: Actions) => VNode;
-  export type emitFunction<Data> = (event: string, data?: Data) => Data;
-  export type vnodeFunction = (props: VNodeData, children: string | VNode | Array<VNode>) => VNode;
+  export type viewFunction<State, Actions> = (
+    state: State,
+    actions: Actions
+  ) => VNode
+  export type emitFunction<Data> = (event: string, data?: Data) => Data
+  export type vnodeFunction = (
+    props: VNodeData,
+    children: string | VNode | Array<VNode>
+  ) => VNode
 
   export interface Actions<State> {
-    [key: string]: ( (state?: State, actions?: Actions<State>, data?: any, emit?: emitFunction<any>) => Partial<State> | any )
-                 | Actions<State>;
+    [key: string]:
+      | ((
+          state?: State,
+          actions?: Actions<State>,
+          data?: any,
+          emit?: emitFunction<any>
+        ) => Partial<State> | any)
+      | Actions<State>
   }
 
-  export interface Events<State, ActionsInterface extends Actions<State> | undefined> {
-    loaded: ( (state: State, actions: ActionsInterface, data: any, emit: emitFunction<any>) => void )
-          | Array< (state: State, actions: ActionsInterface, data: any, emit: emitFunction<any>) => void >;
-    action: ( <Data>(state: State, actions: ActionsInterface, data: Data, emit: emitFunction<any>) => Data )
-          | Array< <Data>(state: State, actions: ActionsInterface, data: Data, emit: emitFunction<any>) => Data >;
-    update: ( (state: State, actions: ActionsInterface, data: State, emit: emitFunction<any>) => State )
-          | Array< (state: State, actions: ActionsInterface, data: State, emit: emitFunction<any>) => State >;
-    render: ( (state: State, actions: ActionsInterface, view: viewFunction<State, ActionsInterface>, emit: emitFunction<any>) => viewFunction<State, ActionsInterface> )
-          | Array<(state: State, actions: ActionsInterface, view: viewFunction<State, ActionsInterface>, emit: emitFunction<any>) => viewFunction<State, ActionsInterface>>;
+  export interface Events<
+    State,
+    ActionsInterface extends Actions<State> | undefined
+  > {
+    loaded:
+      | ((
+          state: State,
+          actions: ActionsInterface,
+          data: any,
+          emit: emitFunction<any>
+        ) => void)
+      | Array<
+          (
+            state: State,
+            actions: ActionsInterface,
+            data: any,
+            emit: emitFunction<any>
+          ) => void
+        >
+    action:
+      | (<Data>(
+          state: State,
+          actions: ActionsInterface,
+          data: Data,
+          emit: emitFunction<any>
+        ) => Data)
+      | Array<
+          <Data>(
+            state: State,
+            actions: ActionsInterface,
+            data: Data,
+            emit: emitFunction<any>
+          ) => Data
+        >
+    update:
+      | ((
+          state: State,
+          actions: ActionsInterface,
+          data: State,
+          emit: emitFunction<any>
+        ) => State)
+      | Array<
+          (
+            state: State,
+            actions: ActionsInterface,
+            data: State,
+            emit: emitFunction<any>
+          ) => State
+        >
+    render:
+      | ((
+          state: State,
+          actions: ActionsInterface,
+          view: viewFunction<State, ActionsInterface>,
+          emit: emitFunction<any>
+        ) => viewFunction<State, ActionsInterface>)
+      | Array<
+          (
+            state: State,
+            actions: ActionsInterface,
+            view: viewFunction<State, ActionsInterface>,
+            emit: emitFunction<any>
+          ) => viewFunction<State, ActionsInterface>
+        >
 
     // [key: string]:( <Data>(state: State, actions: ActionsInterface, data: Data, emit: emitFunction<any>) => Data )
     //      | Array< <Data>(state: State, actions: ActionsInterface, data: Data, emit: emitFunction<any>) => Data >;
   }
 
-  export interface Mixin<State, ActionsInterface extends Actions<State> | undefined, EventsInterface extends Partial<Events<State, ActionsInterface>> | undefined> {
-    state: State;
-    actions: ActionsInterface;
-    events: EventsInterface;
-    mixins: Array< Partial<Mixin<State, ActionsInterface, EventsInterface>> >;
+  export interface Mixin<
+    State,
+    ActionsInterface extends Actions<State> | undefined,
+    EventsInterface extends Partial<Events<State, ActionsInterface>> | undefined
+  > {
+    state: State
+    actions: ActionsInterface
+    events: EventsInterface
+    mixins: Array<Partial<Mixin<State, ActionsInterface, EventsInterface>>>
   }
 
-  export interface Application<State, ActionsInterface extends Actions<State> | undefined, EventsInterface extends Partial<Events<State, ActionsInterface>> | undefined> extends Partial<Mixin<State, ActionsInterface, EventsInterface>> {
-    root: HTMLElement;
-    view: viewFunction<State, ActionsInterface>;
+  export interface Application<
+    State,
+    ActionsInterface extends Actions<State> | undefined,
+    EventsInterface extends Partial<Events<State, ActionsInterface>> | undefined
+  > extends Partial<Mixin<State, ActionsInterface, EventsInterface>> {
+    root: HTMLElement
+    view: viewFunction<State, ActionsInterface>
   }
 
-  export function h(tag: string | vnodeFunction, data?: VNodeData, children?: Array<number | string | VNode>): VNode;
-  export function h(tag: string | vnodeFunction, data?: VNodeData, ...children: Array<number | string | VNode >): VNode;
+  export function h(
+    tag: string | vnodeFunction,
+    data?: VNodeData,
+    children?: Array<number | string | VNode>
+  ): VNode
+  export function h(
+    tag: string | vnodeFunction,
+    data?: VNodeData,
+    ...children: Array<number | string | VNode>
+  ): VNode
 
-  export function app<State, ActionsInterface extends Actions<State> | undefined, EventsInterface extends Partial<Events<State, ActionsInterface>> | undefined>(props: Partial<Application<State, ActionsInterface, EventsInterface>>): emitFunction<any>;
+  export function app<
+    State,
+    ActionsInterface extends Actions<State> | undefined,
+    EventsInterface extends Partial<Events<State, ActionsInterface>> | undefined
+  >(
+    props: Partial<Application<State, ActionsInterface, EventsInterface>>
+  ): emitFunction<any>
 
-  global {
+  namespace global {
     namespace JSX {
-      interface Element extends VNode { }
+      interface Element extends VNode {}
       interface IntrinsicElements {
-        [elemName: string]: VNodeData;
+        [elemName: string]: VNodeData
       }
     }
   }
-
 }

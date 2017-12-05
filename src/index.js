fdb37218026c61311ab416251fdd4f561f51d70e
w-mcilhagga <william.mcilhagga@gmail.com>
2017-12-05 14:20:59 +0000

    changed html tag to body

--- src/index.js
+++ src/index.js
@@ -322,3 +322,327 @@ export function app(props, container) {
     }
   }
 }
+export function h(type, props) {
+  var node
+  var stack = []
+  var children = []
+
+  for (var i = arguments.length; i-- > 2; ) {
+    stack.push(arguments[i])
+  }
+
+  while (stack.length) {
+    if (Array.isArray((node = stack.pop()))) {
+      for (i = node.length; i--; ) {
+        stack.push(node[i])
+      }
+    } else if (null == node || node === true || node === false) {
+    } else {
+      children.push(typeof node === "number" ? (node = node + "") : node)
+    }
+  }
+
+  return typeof type === "string"
+    ? {
+        type: type,
+        props: props || {},
+        children: children
+      }
+    : type(props || {}, children)
+}
+
+export function app(props, container) {
+  var lock
+  var root = (container = container || document.body).children[0]
+  var node = vnode(root, [].map)
+  var lifecycle = []
+  var appState = props.state || {}
+  var appActions = {}
+
+  repaint(init(appState, appActions, props.actions, []))
+
+  return appActions
+
+  function vnode(element, map) {
+    return (
+      element &&
+      h(
+        element.tagName.toLowerCase(),
+        {},
+        map.call(element.childNodes, function(element) {
+          return element.nodeType === 3
+            ? element.nodeValue
+            : vnode(element, map)
+        })
+      )
+    )
+  }
+
+  function set(to, from) {
+    for (var i in from) {
+      to[i] = from[i]
+    }
+    return to
+  }
+
+  function merge(to, from) {
+    return set(set({}, to), from)
+  }
+
+  function setDeep(path, value, from) {
+    var to = {}
+    return 0 === path.length
+      ? value
+      : ((to[path[0]] =
+          1 < path.length
+            ? setDeep(path.slice(1), value, from[path[0]])
+            : value),
+        merge(from, to))
+  }
+
+  function get(path, from) {
+    for (var i = 0; i < path.length; i++) {
+      from = from[path[i]]
+    }
+    return from
+  }
+
+  function isFunction(any) {
+    return "function" === typeof any
+  }
+
+  function init(state, actions, from, path) {
+    for (var key in from) {
+      isFunction(from[key])
+        ? (function(key, action) {
+            actions[key] = function(data) {
+              state = get(path, appState)
+
+              if (
+                isFunction((data = action(data))) &&
+                isFunction((data = data(state)))
+              ) {
+                data = data(actions)
+              }
+
+              if (data && data !== state && !data.then) {
+                repaint(
+                  (appState = setDeep(path, merge(state, data), appState))
+                )
+              }
+
+              return data
+            }
+          })(key, from[key])
+        : init(
+            state[key] || (state[key] = {}),
+            (actions[key] = {}),
+            from[key],
+            path.concat(key)
+          )
+    }
+  }
+
+  function getKey(node) {
+    if (node && node.props) {
+      return node.props.key
+    }
+  }
+
+  function setElementProp(element, name, value, oldValue) {
+    if (name === "key") {
+    } else if (name === "style") {
+      for (var i in merge(oldValue, (value = value || {}))) {
+        element.style[i] = null == value[i] ? "" : value[i]
+      }
+    } else {
+      try {
+        element[name] = null == value ? "" : value
+      } catch (_) {}
+
+      if (!isFunction(value)) {
+        if (null == value || false === value) {
+          element.removeAttribute(name)
+        } else {
+          element.setAttribute(name, value)
+        }
+      }
+    }
+  }
+
+  var namespaces = {
+    svg: "http://www.w3.org/2000/svg", 
+    mathml: "http://www.w3.org/1998/Math/MathML",
+    body: "http://www.w3.org/1999/xhtml"
+  }
+
+  function createElement(node, ns) {
+    if (typeof node === "string") {
+      var element = document.createTextNode(node)
+    } else {
+      ns = ns || namespaces[node.type]
+      var element = ns
+        ? document.createElementNS(ns, node.type)
+        : document.createElement(node.type)
+
+      if (node.props.oncreate) {
+        lifecycle.push(function() {
+          node.props.oncreate(element)
+        })
+      }
+
+      for (var i = 0; i < node.children.length; i++) {
+        element.appendChild(createElement(node.children[i], ns))
+      }
+
+      for (var i in node.props) {
+        setElementProp(element, i, node.props[i])
+      }
+    }
+    return element
+  }
+
+  function updateElement(element, oldProps, props) {
+    for (var i in merge(oldProps, props)) {
+      var value = props[i]
+      var oldValue = i === "value" || i === "checked" ? element[i] : oldProps[i]
+
+      if (value !== oldValue) {
+        setElementProp(element, i, value, oldValue)
+      }
+    }
+
+    if (props.onupdate) {
+      lifecycle.push(function() {
+        props.onupdate(element, oldProps)
+      })
+    }
+  }
+
+  function removeElement(parent, element, props) {
+    function done() {
+      parent.removeChild(element)
+    }
+
+    if (props && props.onremove) {
+      props.onremove(element, done)
+    } else {
+      done()
+    }
+  }
+
+  function patch(parent, element, oldNode, node, ns, nextSibling) {
+    if (oldNode === node) {
+    } else if (null == oldNode) {
+      element = parent.insertBefore(createElement(node, ns), element)
+    } else if (node.type != null && node.type === oldNode.type) {
+      updateElement(element, oldNode.props, node.props)
+
+      ns = ns || namespaces[node.type]
+
+      var len = node.children.length
+      var oldLen = oldNode.children.length
+      var oldKeyed = {}
+      var oldElements = []
+      var keyed = {}
+
+      for (var i = 0; i < oldLen; i++) {
+        var oldElement = (oldElements[i] = element.childNodes[i])
+        var oldChild = oldNode.children[i]
+        var oldKey = getKey(oldChild)
+
+        if (null != oldKey) {
+          oldKeyed[oldKey] = [oldElement, oldChild]
+        }
+      }
+
+      var i = 0
+      var j = 0
+
+      while (j < len) {
+        var oldElement = oldElements[i]
+        var oldChild = oldNode.children[i]
+        var newChild = node.children[j]
+
+        var oldKey = getKey(oldChild)
+        if (keyed[oldKey]) {
+          i++
+          continue
+        }
+
+        var newKey = getKey(newChild)
+        var keyedNode = oldKeyed[newKey] || []
+
+        if (null == newKey) {
+          if (null == oldKey) {
+            patch(element, oldElement, oldChild, newChild, ns)
+            j++
+          }
+          i++
+        } else {
+          if (oldKey === newKey) {
+            patch(element, keyedNode[0], keyedNode[1], newChild, ns)
+            i++
+          } else if (keyedNode[0]) {
+            element.insertBefore(keyedNode[0], oldElement)
+            patch(element, keyedNode[0], keyedNode[1], newChild, ns)
+          } else {
+            patch(element, oldElement, null, newChild, ns)
+          }
+
+          j++
+          keyed[newKey] = newChild
+        }
+      }
+
+      while (i < oldLen) {
+        var oldChild = oldNode.children[i]
+        var oldKey = getKey(oldChild)
+        if (null == oldKey) {
+          removeElement(element, oldElements[i], oldChild.props)
+        }
+        i++
+      }
+
+      for (var i in oldKeyed) {
+        var keyedNode = oldKeyed[i]
+        var reusableNode = keyedNode[1]
+        if (!keyed[reusableNode.props.key]) {
+          removeElement(element, keyedNode[0], reusableNode.props)
+        }
+      }
+    } else if (element && node !== element.nodeValue) {
+      if (typeof node === "string" && typeof oldNode === "string") {
+        element.nodeValue = node
+      } else {
+        element = parent.insertBefore(
+          createElement(node, ns),
+          (nextSibling = element)
+        )
+        removeElement(parent, nextSibling, oldNode.props)
+      }
+    }
+
+    return element
+  }
+
+  function render(next) {
+    lock = !lock
+
+    if (isFunction((next = props.view(appState)))) {
+      next = next(appActions)
+    }
+
+    if (!lock) {
+      root = patch(container, root, node, (node = next))
+    }
+
+    while ((next = lifecycle.pop())) next()
+  }
+
+  function repaint() {
+    if (props.view && !lock) {
+      setTimeout(render, (lock = !lock))
+    }
+  }
+}

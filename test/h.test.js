9aff42be5e858e4d73dc8705b872121d16790d35
Wolfgang <okwolf@users.noreply.github.com>
2017-12-31 02:16:31 -0500

    handle text nodes as part of patch instead of converting to  a string in h (#520)

--- test/h.test.js
+++ test/h.test.js
@@ -32,7 +32,7 @@ test("positional String/Number children", () => {
   expect(h("div", {}, 0, "foo", 1, "baz", 2)).toEqual({
     name: "div",
     props: {},
-    children: ["0", "foo", "1", "baz", "2"]
+    children: [0, "foo", 1, "baz", 2]
   })
 
   expect(h("div", {}, "foo", h("div", {}, "bar"), "baz", "quux")).toEqual({

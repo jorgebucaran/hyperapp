48ed91d65c2dcd64ac9e59cd401f844440a39e58
Albert Groothedde <albertgroothedde@gmail.com>
2018-01-08 13:32:12 +0100

    WiP: refactor typings to allow for better intellisense and support in Typescript

--- hyperapp.d.ts
+++ hyperapp.d.ts
@@ -51,38 +51,78 @@ export function h<Props>(
 
 /** @namespace [App] */
 
+/**
+ * Type to describe a nested map of type T
+ * 
+ * example: { 'foo' : { 'bar': 1 }, 'baz': '' }
+ */
+export type NestedMap<T> = {
+  [key: string]: NestedMap<T> | T
+}
+
+/**
+ * Type to describe anything that's not a function or object
+ */
+export type Primitive = string | number | boolean | null | any[]
+
 /** The result of an action.
  *
  * @memberOf [App]
  */
-export type ActionResult<State> = Partial<State> | Promise<any> | null | void
-
-/** The interface for a single action implementation.
- *
- * @memberOf [App]
- */
-export type ActionType<State, Actions> = (
-  data?: any
-) =>
-  | ((state: State, actions: Actions) => ActionResult<State>)
-  | ActionResult<State>
-
-/** The interface for actions implementations.
- *
- * @memberOf [App]
- */
-export type ActionsType<State, Actions> = {
-  [P in keyof Actions]:
-    | ActionType<State, Actions>
-    | ActionsType<any, Actions[P]>
-}
+export type ActionResult<TState extends Primitive | NestedMap<Primitive>> =
+  | Partial<TState>
+  | Promise<any>
+  | null
+  | void
 
 /** The view function describes the application UI as a tree of VNodes.
  * @returns A VNode tree.
  * @memberOf [App]
  */
-export interface View<State, Actions> {
-  (state: State, actions: Actions): VNode<object>
+export interface View<
+  TState extends Primitive | NestedMap<Primitive>,
+  TActions extends NestedMap<WiredAction<any, TState>>
+> {
+  (state: TState, actions: TActions): VNode<object>
+}
+
+/**
+ * The action that's being passed to actions and returned from app()
+ */
+type WiredAction<
+  TPayload extends Primitive | NestedMap<Primitive>,
+  TState extends Primitive | NestedMap<Primitive>
+> = (data?: TPayload) => TState
+
+/**
+ * The action that's being passed to the actions object in app. 
+ * Use this for implementing actions.
+ */
+export type ActionType<
+  TState extends Primitive | NestedMap<Primitive> = any,
+  TActions extends
+    | NestedMap<WiredActions<TState, any>>
+    | NestedMap<UnwiredActions<TState, any>> = any,
+  TPayload extends Primitive | NestedMap<Primitive> = any
+> = (
+  data?: TPayload
+) =>
+  | ((state: TState, actions: TActions) => ActionResult<TState>)
+  | ActionResult<TState>
+
+/**
+ * The interface for app(). Use this for implementing Higher Order App's
+ */
+export interface App {
+  <
+    TState extends Primitive | NestedMap<Primitive>,
+    TActions extends UnwiredActions<TState, TActions>
+  >(
+    state: TState,
+    actions: TActions,
+    view: View<TState, TActions>,
+    container: HTMLElement | null
+  ): { [P in keyof TActions]: TActions[P] }
 }
 
 /** The app() call creates and renders a new application.
@@ -92,17 +132,33 @@ export interface View<State, Actions> {
  * @param view The view function.
  * @param container The DOM element where the app will be rendered to.
  * @returns The actions wired to the application.
- * @memberOf [App]
  */
-export function app<State, Actions>(
-  state: State,
-  actions: ActionsType<State, Actions>,
-  view: View<State, Actions>,
-  container: Element | null
-): Actions
+export declare const app: App
+
+/** 
+ * Convenience type for State
+ */
+export interface StateType extends NestedMap<Primitive> {}
+
+/**
+ * Convenience alias to extend your actions object from, when implementing actions.
+ */
+interface UnwiredActions<
+  TState extends Primitive | NestedMap<Primitive> = any,
+  TActions extends NestedMap<
+    WiredActions<TState, TActions> | UnwiredActions<TState>
+  > = any
+> extends NestedMap<any> {}
+
+/**
+ * Convenience alias for NestedMap<WiredAction>
+ */
+interface WiredActions<
+  TState extends Primitive | NestedMap<Primitive>,
+  TActions extends UnwiredActions
+> extends NestedMap<WiredAction<any, TState>> {}
 
 /** @namespace [JSX] */
-
 declare global {
   namespace JSX {
     interface Element<Data> extends VNode<object> {}

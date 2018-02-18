3c7b08830fd48b3b8399926e182350da73e9c011
Vladimir Kutepov <frenzzy.man@gmail.com>
2018-02-19 01:15:26 +0700

    Lazy components

--- src/index.js
+++ src/index.js
@@ -11,19 +11,17 @@ export function h(name, attributes /*, ...rest*/) {
       for (length = node.length; length--; ) {
         rest.push(node[length])
       }
-    } else if (node != null && node !== true && node !== false) {
+    } else {
       children.push(node)
     }
   }
 
-  return typeof name === "function"
-    ? name(attributes || {}, children)
-    : {
-        nodeName: name,
-        attributes: attributes || {},
-        children: children,
-        key: attributes && attributes.key
-      }
+  return {
+    nodeName: name,
+    attributes: attributes || {},
+    children: children,
+    key: attributes && attributes.key
+  }
 }
 
 export function app(state, actions, view, container) {
@@ -51,12 +49,27 @@ export function app(state, actions, view, container) {
     }
   }
 
+  function getVNode(node) {
+    if (node == null || typeof node === "boolean") {
+      return ""
+    }
+    if (typeof node.nodeName === "function") {
+      return getVNode(node.nodeName(node.attributes, node.children))
+    }
+    return node
+  }
+
   function render() {
     renderLock = !renderLock
 
     var next = view(globalState, wiredActions)
     if (container && !renderLock) {
-      rootElement = patch(container, rootElement, oldNode, (oldNode = next))
+      rootElement = patch(
+        container,
+        rootElement,
+        oldNode,
+        (oldNode = getVNode(next))
+      )
       firstRender = false
     }
 
@@ -168,7 +181,9 @@ export function app(state, actions, view, container) {
       }
 
       for (var i = 0; i < node.children.length; i++) {
-        element.appendChild(createElement(node.children[i], isSVG))
+        element.appendChild(
+          createElement((node.children[i] = getVNode(node.children[i])), isSVG)
+        )
       }
 
       for (var name in node.attributes) {
@@ -262,7 +277,7 @@ export function app(state, actions, view, container) {
 
       while (j < node.children.length) {
         var oldChild = oldNode.children[i]
-        var newChild = node.children[j]
+        var newChild = (node.children[j] = getVNode(node.children[j]))
 
         var oldKey = getKey(oldChild)
         var newKey = getKey(newChild)

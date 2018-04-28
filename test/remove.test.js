527eff7bce0563d59f8e62b269c09a5f9b8ffaee
joan xie <w4ergf@qq.com>
2018-04-28 17:28:45 +0800

    Update tests

--- test/remove.test.js
+++ test/remove.test.js
@@ -1,52 +1,67 @@
 import { h, app } from "../src"
+var changeApp = (changer, ...args) =>
+  new Promise(resolve => {
+    changer && changer.apply(null, args)
+    setTimeout(resolve, 100)
+  })
 test("onremove reordering", function(done) {
-  var theApp = app(
-    {
-      list: [0, 1, 2, 3]
-    },
-    {
-      change: function() {
-        return function() {
-          return {
-            list: [2, 3]
+  var removers = [],
+    theApp = app(
+      {
+        list: [0, 1, 2, 3]
+      },
+      {
+        change: function() {
+          return function() {
+            return {
+              list: [2, 3]
+            }
+          }
+        },
+        changeAgain: function() {
+          return function() {
+            return {
+              list: [2, 4]
+            }
           }
         }
       },
-      changeAgain: function() {
-        return function() {
-          return {
-            list: [2, 4]
-          }
-        }
-      }
-    },
-    state => {
-      return (
-        <ul>
-          {state.list.map(item => (
-            <li key={item} onremove={(_, f) => setTimeout(f, 800)}>
-              {item}
-            </li>
-          ))}
-        </ul>
+      state => {
+        return (
+          <ul>
+            {state.list.map(item => (
+              <li key={item} onremove={(_, f) => removers.push(f)}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        )
+      },
+      document.body
+    )
+
+  changeApp()
+    .then(() => {
+      expect(document.body.innerHTML).toBe(
+        "<ul><li>0</li><li>1</li><li>2</li><li>3</li></ul>"
       )
-    },
-    document.body
-  )
-  setTimeout(() => {
-    expect(document.body.innerHTML).toBe(
-      "<ul><li>0</li><li>1</li><li>2</li><li>3</li></ul>"
-    )
-    theApp.change()
-  }, 100)
-  setTimeout(() => {
-    expect(document.body.innerHTML).toBe(
-      "<ul><li>0</li><li>1</li><li>2</li><li>3</li></ul>"
-    )
-    theApp.changeAgain()
-  }, 500)
-  setTimeout(() => {
-    expect(document.body.innerHTML).toBe("<ul><li>2</li><li>4</li></ul>")
-  }, 1600)
-  setTimeout(done, 3000)
+      return changeApp(theApp.change)
+    })
+    .then(() => {
+      expect(document.body.innerHTML).toBe(
+        "<ul><li>0</li><li>1</li><li>2</li><li>3</li></ul>"
+      )
+    })
+    .then(() => {
+      removers.forEach(f => f())
+      removers = []
+      expect(document.body.innerHTML).toBe("<ul><li>2</li><li>3</li></ul>")
+      return changeApp(theApp.changeAgain)
+    })
+    .then(() => {
+      removers.forEach(f => f())
+      removers = []
+      expect(document.body.innerHTML).toBe("<ul><li>2</li><li>4</li></ul>")
+    })
+    .then(done)
 })

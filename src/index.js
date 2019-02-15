d563302da7945b397bb55a0225388b7ed440e957
Jorge Bucaran <mail@jorgebucaran.com>
2019-02-15 20:07:33 +0900

    Commit dirty tree in order to publish

--- src/index.js
+++ src/index.js
@@ -482,7 +482,7 @@ export var h = function(name, props) {
 }
 
 var cancel = function(sub) {
-  sub.cancel()
+  sub[2]()
 }
 
 var isSameValue = function(a, b) {
@@ -502,9 +502,8 @@ var isSameAction = function(a, b) {
 }
 
 var restart = function(sub, oldSub, dispatch) {
-  for (var k in merge(sub, oldSub)) {
-    if (k === "cancel") {
-    } else if (sub[k] === oldSub[k] || isSameAction(sub[k], oldSub[k])) {
+  for (var k in merge(sub[1], oldSub[1])) {
+    if (sub[1][k] === oldSub[1][k] || isSameAction(sub[1][k], oldSub[1][k])) {
     } else {
       cancel(oldSub)
       return start(sub, dispatch)
@@ -514,23 +513,25 @@ var restart = function(sub, oldSub, dispatch) {
 }
 
 var start = function(sub, dispatch) {
-  return merge(sub, {
-    cancel: sub.effect(sub, dispatch)
-  })
+  return sub.concat(sub[0](sub[1], dispatch))
+}
+
+var isSubscription = function(sub) {
+  return isArray(sub) && typeof sub[0] === "function"
 }
 
 var refresh = function(sub, oldSub, dispatch) {
-  if (isArray(sub) || isArray(oldSub)) {
-    var out = []
-    var subs = isArray(sub) ? sub : [sub]
-    var oldSubs = isArray(oldSub) ? oldSub : [oldSub]
+  // if (isArray(sub) || isArray(oldSub)) {
+  //   var out = []
+  //   var subs = isArray(sub) ? sub : [sub]
+  //   var oldSubs = isArray(oldSub) ? oldSub : [oldSub]
 
-    for (var i = 0; i < subs.length || i < oldSubs.length; i++) {
-      out.push(refresh(subs[i], oldSubs[i], dispatch))
-    }
+  //   for (var i = 0; i < subs.length || i < oldSubs.length; i++) {
+  //     out.push(refresh(subs[i], oldSubs[i], dispatch))
+  //   }
 
-    return out
-  }
+  //   return out
+  // }
 
   return sub
     ? oldSub
@@ -548,7 +549,7 @@ export function app(props) {
   var container = props.container
   var element = container && container.children && container.children[0]
   var lastNode = element && recycleElement(element)
-  var lastSub = []
+  var lastSub
   var updateInProgress = false
 
   var setState = function(newState) {
@@ -562,20 +563,28 @@ export function app(props) {
     }
   }
 
-  var dispatch = function(obj, data) {
-    if (obj == null) {
-    } else if (typeof obj === "function") {
-      dispatch(obj(state, data))
-    } else if (isArray(obj)) {
-      if (typeof obj[0] === "function") {
-        dispatch(obj[0](state, obj[1], data))
-      } else {
-        obj[1].effect(obj[1], dispatch, setState(obj[0]))
-      }
-    } else {
-      setState(obj)
-    }
+var handleActionResult = function(actionResult) {
+  if (isArray(actionResult)) {
+    actionResult[1][0](
+      actionResult[1][1],
+      dispatch,
+      setState(actionResult[0])
+    )
+  } else {
+    dispatch(actionResult)
   }
+}
+
+var dispatch = function(obj, data) {
+  if (obj == null) {
+  } else if (typeof obj === "function") {
+    handleActionResult(obj(state, data))
+  } else if (isArray(obj)) {
+    handleActionResult(obj[0](state, obj[1], data))
+  } else {
+    setState(obj)
+  }
+}
 
   var eventProxy = function(event) {
     dispatch(event.currentTarget.events[event.type], event)

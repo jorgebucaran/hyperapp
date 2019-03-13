ff3142e2fee380d4c7721a3e2c80e77ffede4792
Jorge Bucaran <mail@jorgebucaran.com>
2019-03-13 22:09:51 +0900

    Remove unused test file

--- test/h.test.js
+++
@@ -1,8 +0,0 @@
-import { h } from "../src"
-
-test("empty vnode", () => {
-  const node = h("div")
-  expect(node.name).toEqual("div")
-  expect(node.props).toEqual({})
-  expect(node.children).toEqual([])
-})

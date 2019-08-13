d13ae0e42b4a28d1fbef6fd15791c53a3c61d662
Dice <tetradice@gmail.com>
2019-08-13 19:22:47 +0900

    Remove default payload support from libraries

--- lib/events/events.d.ts
+++ lib/events/events.d.ts
@@ -1,25 +1,23 @@
 import { Subscription, Effect, Action, Dispatchable, ActionWithEffects } from "hyperapp";
 
-type EventArgument<E extends keyof GlobalEventHandlers> = Parameters<NonNullable<GlobalEventHandlers[E]>>[0];
-
-declare function onMouseEnter<S>(action: Dispatchable<S, EventArgument<'onmouseenter'>>): Subscription<S>;
-declare function onMouseLeave<S>(action: Dispatchable<S, EventArgument<'onmouseleave'>>): Subscription<S>;
-declare function onMouseMove<S>(action: Dispatchable<S, EventArgument<'onmousemove'>>): Subscription<S>;
-declare function onMouseOut<S>(action: Dispatchable<S, EventArgument<'onmouseout'>>): Subscription<S>;
-declare function onMouseOver<S>(action: Dispatchable<S, EventArgument<'onmouseover'>>): Subscription<S>;
-declare function onMouseUp<S>(action: Dispatchable<S, EventArgument<'onmouseup'>>): Subscription<S>;
-declare function onTouchStart<S>(action: Dispatchable<S, EventArgument<'ontouchstart'>>): Subscription<S>;
-declare function onTouchMove<S>(action: Dispatchable<S, EventArgument<'ontouchmove'>>): Subscription<S>;
-declare function onTouchEnd<S>(action: Dispatchable<S, EventArgument<'ontouchend'>>): Subscription<S>;
-declare function onKeyDown<S>(action: Dispatchable<S, EventArgument<'onkeydown'>>): Subscription<S>;
-declare function onKeyUp<S>(action: Dispatchable<S, EventArgument<'onkeyup'>>): Subscription<S>;
-declare function onFocus<S>(action: Dispatchable<S, EventArgument<'onfocus'>>): Subscription<S>;
-declare function onBlur<S>(action: Dispatchable<S, EventArgument<'onblur'>>): Subscription<S>;
+declare function onMouseEnter<S>(action: Dispatchable<S>): Subscription<S>;
+declare function onMouseLeave<S>(action: Dispatchable<S>): Subscription<S>;
+declare function onMouseMove<S>(action: Dispatchable<S>): Subscription<S>;
+declare function onMouseOut<S>(action: Dispatchable<S>): Subscription<S>;
+declare function onMouseOver<S>(action: Dispatchable<S>): Subscription<S>;
+declare function onMouseUp<S>(action: Dispatchable<S>): Subscription<S>;
+declare function onTouchStart<S>(action: Dispatchable<S>): Subscription<S>;
+declare function onTouchMove<S>(action: Dispatchable<S>): Subscription<S>;
+declare function onTouchEnd<S>(action: Dispatchable<S>): Subscription<S>;
+declare function onKeyDown<S>(action: Dispatchable<S>): Subscription<S>;
+declare function onKeyUp<S>(action: Dispatchable<S>): Subscription<S>;
+declare function onFocus<S>(action: Dispatchable<S>): Subscription<S>;
+declare function onBlur<S>(action: Dispatchable<S>): Subscription<S>;
 
 declare function eventKey(e: Event): any;
 declare function targetChecked(e: Event): any;
 declare function targetValue(e: Event): any;
 
 declare function eventOptions<S>(props: { preventDefault?: boolean, stopPropagation?: boolean, action?: Dispatchable<S, Event> }): Effect<S, Event>;
-declare function preventDefault<S, CPayload>(action: Dispatchable<S, Event>): ActionWithEffects<S, CPayload, Event>;
-declare function stopPropagation<S, CPayload>(action: Dispatchable<S, Event>): ActionWithEffects<S, CPayload, Event>;
+declare function preventDefault<S, DPayload, CPayload>(action: Dispatchable<S, DPayload>): ActionWithEffects<S, CPayload, DPayload>;
+declare function stopPropagation<S, DPayload, CPayload>(action: Dispatchable<S, DPayload>): ActionWithEffects<S, CPayload, DPayload>;
--- lib/random/random.d.ts
+++ lib/random/random.d.ts
@@ -3,8 +3,5 @@ import { Subscription, Effect, Action, Dispatchable } from "hyperapp";
 type GeneratorFunction<R> = (value: number) => R;
 type TupleGeneratorResult<T extends any[]> = { [K in keyof T]: GeneratorFunction<T[K]> };
 
-export declare function generate<S, R extends any[]>(action: Dispatchable<S, R>, generator: TupleGeneratorResult<R>): Effect<S>;
-export declare function generate<S, R>(action: Dispatchable<S, R>, generator: GeneratorFunction<R>): Effect<S>;
-
-export declare function generateTest<S, R extends any[]>(action: Dispatchable<S, R[]>, generator: TupleGeneratorResult<R>): {type: 1, S: S, R: R};
-export declare function generateTest<S, R>(action: Dispatchable<S, R>, generator: GeneratorFunction<R>): {type: 2, S: S, R: R};
+export declare function generate<S, R extends any[]>(action: Dispatchable<S>, generator: TupleGeneratorResult<R>): Effect<S>;
+export declare function generate<S, R>(action: Dispatchable<S>, generator: GeneratorFunction<R>): Effect<S>;

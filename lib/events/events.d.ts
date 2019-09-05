77277d2bbb28ca054d1128e688481f66cd0aface
Dice <tetradice@gmail.com>
2019-09-06 08:28:41 +0900

    Fix function signatures

--- lib/events/events.d.ts
+++ lib/events/events.d.ts
@@ -1,26 +1,26 @@
 type EventArgument<E extends keyof GlobalEventHandlers> = Parameters<NonNullable<GlobalEventHandlers[E]>>[0];
 
-declare function onMouseEnter<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onmouseenter'>>): hyperappSubset.Subscription<S>;
-declare function onMouseLeave<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onmouseleave'>>): hyperappSubset.Subscription<S>;
-declare function onMouseMove<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onmousemove'>>): hyperappSubset.Subscription<S>;
-declare function onMouseOut<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onmouseout'>>): hyperappSubset.Subscription<S>;
-declare function onMouseOver<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onmouseover'>>): hyperappSubset.Subscription<S>;
-declare function onMouseUp<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onmouseup'>>): hyperappSubset.Subscription<S>;
-declare function onTouchStart<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'ontouchstart'>>): hyperappSubset.Subscription<S>;
-declare function onTouchMove<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'ontouchmove'>>): hyperappSubset.Subscription<S>;
-declare function onTouchEnd<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'ontouchend'>>): hyperappSubset.Subscription<S>;
-declare function onKeyDown<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onkeydown'>>): hyperappSubset.Subscription<S>;
-declare function onKeyUp<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onkeyup'>>): hyperappSubset.Subscription<S>;
-declare function onFocus<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onfocus'>>): hyperappSubset.Subscription<S>;
-declare function onBlur<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onblur'>>): hyperappSubset.Subscription<S>;
+export declare function onMouseEnter<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onmouseenter'>>): hyperappSubset.Subscription<S>;
+export declare function onMouseLeave<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onmouseleave'>>): hyperappSubset.Subscription<S>;
+export declare function onMouseMove<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onmousemove'>>): hyperappSubset.Subscription<S>;
+export declare function onMouseOut<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onmouseout'>>): hyperappSubset.Subscription<S>;
+export declare function onMouseOver<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onmouseover'>>): hyperappSubset.Subscription<S>;
+export declare function onMouseUp<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onmouseup'>>): hyperappSubset.Subscription<S>;
+export declare function onTouchStart<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'ontouchstart'>>): hyperappSubset.Subscription<S>;
+export declare function onTouchMove<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'ontouchmove'>>): hyperappSubset.Subscription<S>;
+export declare function onTouchEnd<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'ontouchend'>>): hyperappSubset.Subscription<S>;
+export declare function onKeyDown<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onkeydown'>>): hyperappSubset.Subscription<S>;
+export declare function onKeyUp<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onkeyup'>>): hyperappSubset.Subscription<S>;
+export declare function onFocus<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onfocus'>>): hyperappSubset.Subscription<S>;
+export declare function onBlur<S>(action: hyperappSubset.Dispatchable<S, EventArgument<'onblur'>>): hyperappSubset.Subscription<S>;
 
-declare function eventKey(e: Event): any;
-declare function targetChecked(e: Event): any;
-declare function targetValue(e: Event): any;
+export declare function eventKey(e: Event): any;
+export declare function targetChecked(e: Event): any;
+export declare function targetValue(e: Event): any;
 
-declare function eventOptions<S>(props: { preventDefault?: boolean, stopPropagation?: boolean, action?: hyperappSubset.Dispatchable<S, Event> }): hyperappSubset.Effect<S>;
-declare function preventDefault<S>(action: hyperappSubset.Dispatchable<S, Event>): hyperappSubset.Action<S, Event>;
-declare function stopPropagation<S>(action: hyperappSubset.Dispatchable<S, Event>): hyperappSubset.Action<S, Event>;
+export declare function eventOptions<S>(props: { preventDefault?: boolean, stopPropagation?: boolean, action?: hyperappSubset.Dispatchable<S, Event> }): hyperappSubset.Effect<S>;
+export declare function preventDefault<S>(action: hyperappSubset.Dispatchable<S, Event>): hyperappSubset.Action<S, Event>;
+export declare function stopPropagation<S>(action: hyperappSubset.Dispatchable<S, Event>): hyperappSubset.Action<S, Event>;
 
 declare namespace hyperappSubset {
     type PayloadCreator<DPayload, CPayload> = ((data: DPayload) => CPayload);

9976ce41a017a2759d86695db922197db8ce7415
Alexandre Lotte <alexandre.lotte@osedea.com>
2020-02-06 17:39:07 -0500

    Improve docs

--- docs/generate-search-data.js
+++ docs/generate-search-data.js
@@ -2,6 +2,7 @@ const path = require('path')
 const glob = require('glob')
 const fs = require('fs')
 
+// Generate a big JSON file of all the text of all pages, used for search
 glob('src/pages/**/*.md', {}, (err, files) => {
   const data = files.reduce((data, filePath) => {
     const content = fs.readFileSync(path.join(__dirname, filePath), 'utf8')

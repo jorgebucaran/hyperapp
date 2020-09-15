a964fdfa4f1399eef77440bd00edf3f7162c3f0c
Ron Martinez <ramartin.net@gmail.com>
2020-09-15 16:53:40 -0400

    Add type tests for async effects.

--- types/test/effects.test.ts
+++ types/test/effects.test.ts
@@ -11,3 +11,21 @@ const sayHi = (x: EffectData<string>): EffectDescriptor<string, string, string> 
 
 // $ExpectType EffectDescriptor<string, string, string>
 sayHi("hi")
+
+// -----------------------------------------------------------------------------
+
+// Credit: https://gist.github.com/eteeselink/81314282c95cd692ea1d
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+// $ExpectType Effect<string, string, string>
+const runSayHiEventually = (async (dispatch, data) => {
+  await delay (5000)
+  console.log(data)
+  window.requestAnimationFrame(() => dispatch((state, x) => state + x, data))
+}) as Effect<string, string, string>
+
+const sayHiEventually = (x: EffectData<string>): EffectDescriptor<string, string, string> =>
+  [runSayHiEventually, x]
+
+// $ExpectType EffectDescriptor<string, string, string>
+sayHiEventually("hi")

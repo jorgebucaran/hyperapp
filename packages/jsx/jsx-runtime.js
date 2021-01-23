3768619959078acff17c9b1016fb3e1259b41d85
loteoo <alexandre.lotte@osedea.com>
2021-01-23 16:17:24 -0500

    Rebase and fix PR comments

---
+++ packages/jsx/jsx-runtime.js
@@ -0,0 +1,16 @@
+import { h, text } from "hyperapp"
+
+const jsx = (tag, { children = [], ...props = {} }) =>
+  typeof tag === "function"
+    ? tag(props, children)
+    : h(
+        tag,
+        props,
+        []
+          .concat(children)
+          .map((any) =>
+            typeof any === "string" || typeof any === "number" ? text(any) : any
+          )
+      )
+
+export { jsx, jsx as jsxs, jsx as jsxDEV }

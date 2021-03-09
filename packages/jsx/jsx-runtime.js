4cb9bf4bc63fe61a064aff33677d7d0354ffe087
Alexandre Lotte <alexandre.lotte@osedea.com>
2021-03-09 00:02:56 -0500

    Refactor jsx-runtime function, add support for fragments

--- packages/jsx/jsx-runtime.js
+++ packages/jsx/jsx-runtime.js
@@ -2,15 +2,36 @@ import { h, text } from "hyperapp"
 
 const textTypes = ["string", "number", "bigint"]
 
-const childNode = (child) =>
-  textTypes.includes(typeof child) ? text(child) : child
+/**
+ * Turn text elements into text nodes.
+ * Leave other elements intact (ex: functions, dates, objects, etc)
+ */
+const textify = (children) =>
+  textTypes.includes(typeof children) ? text(children) : children
 
-export const jsx = (type, { children, ...props }, key) =>
-  typeof type === "function"
-    ? type({ ...props, key }, childNode(children))
-    : h(type, { ...props, key }, childNode(children))
+/**
+ * Apply textify function to all children (one or many)
+ */
+const textifyArray = (children) => [].concat(children).flatMap(textify)
 
-export const jsxs = (type, { children, ...props }, key) =>
-  typeof type === "function"
-    ? type({ ...props, key }, childNode(children))
-    : h(type, { ...props, key }, children.flatMap(childNode))
+/**
+ * Add JSX compatibility to h function
+ */
+const createVNode = (type, { children, ...props }, key) => {
+  const childNodes = textifyArray(children)
+  return typeof type === "function"
+    ? type({ ...props, key }, childNodes)
+    : h(type, { ...props, key }, childNodes)
+}
+
+/**
+ * Support fragments
+ */
+const Fragment = (_props, children) => textifyArray(children)
+
+export {
+	createVNode as jsx,
+	createVNode as jsxs,
+	createVNode as jsxDEV,
+	Fragment
+};

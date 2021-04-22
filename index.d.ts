177d83e7ab2e6021d31e1b11e61aaf31e2fb1953
Ron Martinez <ramartin.net@gmail.com>
2021-04-22 04:03:33 -0400

    Better direct usage of EventActions (#1054)

--- index.d.ts
+++ index.d.ts
@@ -164,17 +164,18 @@ declare module "hyperapp" {
   type Key = string | null | undefined
 
   // Virtual DOM properties will often correspond to HTML attributes.
-  type PropList<S> = Readonly<
-    Partial<
-      Omit<
-        HTMLElement,
-        keyof (DocumentAndElementEventHandlers &
+  type PropList<S> =
+    Readonly<
+      Partial<
+        Omit<HTMLElement, keyof (
+          DocumentAndElementEventHandlers &
           ElementCSSInlineStyle &
-          GlobalEventHandlers)
-      >
-    > &
-      ElementCreationOptions &
-      EventActions<S> & {
+          GlobalEventHandlers
+        )> &
+        ElementCreationOptions &
+        EventActions<S>
+      > &
+      {
         [_: string]: unknown
         class?: ClassProp
         key?: Key
@@ -184,7 +185,7 @@ declare module "hyperapp" {
         // are not mistaken for also having `PropList`.
         _VDOM?: never
       }
-  >
+    >
 
   // The `class` property represents an HTML class attribute string.
   type ClassProp =
@@ -210,7 +211,7 @@ declare module "hyperapp" {
 
   // Event handlers are implemented using actions.
   type EventActions<S> = {
-    [K in keyof EventsMap]?:
+    [K in keyof EventsMap]:
       | Action<S, EventsMap[K]>
       | [action: Action<S>, payload: any]
   }
